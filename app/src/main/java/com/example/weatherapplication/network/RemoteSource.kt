27a92cefe6b411b775b816830e6a096fc6db9efe
mohamed taha93 <mohamedtahadawoud@gmail.com>
2023-06-29 09:40:10 +0300

    Revert "s"
    This reverts commit f3b7fe6f7d612691360563c2ebc162a3bc174897.

--- app/src/main/java/com/example/weatherapplication/network/RemoteSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.designpattern.network
-
-import kotlinx.coroutines.flow.Flow
-import retrofit2.Response
-
-interface RemoteSource {
-    suspend fun getProductsFromNetwork(): Flow<Response<com.example.designpattern.model.Response>>
-
-}
