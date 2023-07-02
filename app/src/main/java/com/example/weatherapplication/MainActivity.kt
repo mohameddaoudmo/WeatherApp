aaaf9849d416c52e89b249c376648b0a1a471ccb
mohamed taha93 <mohamedtahadawoud@gmail.com>
2023-07-02 06:54:12 +0200

    commi

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -1,32 +1,44 @@
 package com.example.weatherapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.weatherapplication.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
+    lateinit var binding : ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot())
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         navigationView = findViewById<NavigationView>(R.id.nav_view)
-        val actionBar = supportActionBar
-        actionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
-        actionBar!!.setDisplayShowHomeEnabled(true)
-        actionBar.setDisplayHomeAsUpEnabled(true)
+//        val actionBar = supportActionBar
+//        actionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+//        actionBar!!.setDisplayShowHomeEnabled(true)
+//        actionBar.setDisplayHomeAsUpEnabled(true)
 
         val navController = findNavController(this, R.id.nav_host_fragment)
-        setupWithNavController(navigationView, navController)
+//        setupWithNavController(navigationView, navController)
+        setupWithNavController(binding.bottomNavigation, navController)
+
 
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -41,5 +53,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
+    }
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For the content to be visible behind the status and navigation bar,
+        // your theme needs to be Theme.AppCompat.Light.NoActionBar.
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
     }
 }
